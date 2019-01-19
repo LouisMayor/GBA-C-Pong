d03539cc19205da72af43028f8ac45bbc4fdf4c1
LouisMayor <louismayor1@gmail.com>
2019-01-19 20:10:49 +0000

    added controllable paddle, and removed various bugs(missing brace on palette sprite memory offset)

--- Main.c
+++ Main.c
@@ -2,26 +2,91 @@
 #include <stdlib.h>
 #include "include/macros.h"
 #include "include/types.h"
+#include "include/utility.h"
 
 // forward decl
 void SetPosition( volatile obj_attributes* object, int x, int y);
 
 int main(void) {
-
-	REG_DISPLAY = 0x1000 | 0x0040;
-
-	REG_VIDEOMODE = 0x03; // Use video mode 3 (in BG2, a 16bpp bitmap in VRAM)
-	REG_BGMODE = 0x04; // Enable BG2 (BG0 = 1, BG1 = 2, BG2 = 4, ...)
 	
-	while(1) {
-		// REG_DISPLAY_VCOUNT - V-BLANK i.e. update AFTER screen has been fully drawn. V-DRAW i.e. draw
-		while( REG_DISPLAY_VCOUNT >= 160 ); // V-BLANK - 0 -> 227
-		while( REG_DISPLAY_VCOUNT < 160 ); // V-DRAW
+	// Writing sprites in VRAM. Paddle and ball
+	volatile uint16* paddleTile = (uint16*)TILE_LAYOUT[4][1];
+	volatile uint16* ballTile = (uint16*)TILE_LAYOUT[4][5];
+
+	// writing 4 pixels of colour into index 1
+	for( int i = 0; i < 4 * (sizeof(tile_4bpp) * 0.5); ++i ) {
+		paddleTile[i] = 0x1111;
 	}
 
+	// writing 4 pixels of colour into index 2
+	for( int i = 0; i < sizeof(tile_4bpp) * 0.5; ++i ) {
+		ballTile[i] = 0x2222;
+	}
+
+	// writing the colour palette for each sprite (into the first palette, i.e. 16 colours)
+	OBJ_PALETTE_LAYOUT[1] = RGB15(0x1F, 0x1F, 0x1F);
+	OBJ_PALETTE_LAYOUT[2] = RGB15(0x1F, 0x00, 0x1F);
+
+	// pass sprite data into object attribute memory
+	volatile obj_attributes* paddleAttributes = &OAM_ATTR_LAYOUT[0];
+	paddleAttributes->attr0 = 0x8000;
+	paddleAttributes->attr1 = 0x4000;
+	paddleAttributes->attr2 = 1;
+
+	volatile obj_attributes* ballAttributes = &OAM_ATTR_LAYOUT[1];
+	ballAttributes->attr0 = 0;
+	ballAttributes->attr1 = 0;
+	ballAttributes->attr2 = 5;
+
+	const int paddleWidth = 8;
+	const int paddleHeight = 32;
+
+	const int ballWidth = 8;
+	const int ballHeight = 8;
+
+	int paddleVelocity = 2;
+	int ballVelocityX = 2;
+	int ballVelocityY = 1;
+
+	int paddleX = 5;
+	int paddleY = 96;
+
+	int ballX = 22;
+	int ballY = 96;
+
+	const int paddleMaxY = HEIGHT - paddleHeight;
+
+	SetPosition(paddleAttributes, paddleX, paddleY);
+	SetPosition(ballAttributes, ballX, ballY);
+
+	REG_DISPLAY = 0x1000 | 0x0040;
+	
+	uint32 keyState = 0;
+	while(1) {
+		// REG_DISPLAY_VCOUNT - V-BLANK i.e. update AFTER screen has been fully drawn. V-DRAW i.e. draw
+		//  0 -> 227
+		while( REG_DISPLAY_VCOUNT >= 160 ); // V-BLANK
+		while( REG_DISPLAY_VCOUNT < 160 ); // V-DRAW
+
+		// grab current key state
+		keyState = ~REG_INPUT & KEY_ANY;
+
+		if ( keyState & KEY_UP || keyState & KEY_DOWN ) {
+			if( keyState & KEY_UP ) {
+				paddleY = ClampInt( paddleY - paddleVelocity, 0, paddleMaxY );
+			}			
+			else if( keyState & KEY_DOWN ) {
+				paddleY = ClampInt( paddleY + paddleVelocity, 0, paddleMaxY );
+			}
+			SetPosition(paddleAttributes, paddleX, paddleY);
+		}
+
+		SetPosition(ballAttributes, ballX, ballY);
+	}
 	return 0;
 }
 
 void SetPosition( volatile obj_attributes* object, int x, int y) {
-
+	object->attr0 = (object->attr0 & ~OBJ_MASK_ATTR0_Y) | (y & OBJ_MASK_ATTR0_Y);
+	object->attr1 = (object->attr1 & ~OBJ_MASK_ATTR1_X) | (x & OBJ_MASK_ATTR1_X);
 }
--- include/macros.h
+++ include/macros.h
@@ -10,36 +10,36 @@
 // GBA data
 
 // HWD addresses
-#define EXE_ONLY_ROM 	0x00000000
-#define EWRAM 			0x02000000
-#define IWRAM 			0x03000000
-#define IO 				0x04000000
-#define COL_PAL 		0x05000000
-#define VRAM 			0x06000000
-#define OAM 			0x07000000
-#define PAK_ROM 		0x08000000
-#define PAK_RAM 		0x0E000000
+#define EXE_ONLY_ROM	0x00000000
+#define EWRAM			0x02000000
+#define IWRAM			0x03000000
+#define IO				0x04000000
+#define COL_PAL			0x05000000
+#define VRAM			0x06000000
+#define OAM				0x07000000
+#define PAK_ROM			0x08000000
+#define PAK_RAM			0x0E000000
 
 // Registers				(*(type)ptr)
 #define REG_DISPLAY			(*((volatile uint32*)(IO)))
-#define REG_DISPLAY_VCOUNT 	(*((volatile uint32*)(IO + 0x0006)))
+#define REG_DISPLAY_VCOUNT	(*((volatile uint32*)(IO + 0x0006)))
 #define REG_VIDEOMODE		(*((volatile uint32*)(IO + 0x0004)))
 #define REG_BGMODE			(*((volatile uint32*)(IO + 0x0008)))
 #define REG_VRAM			(*((volatile uint16*)(VRAM)))
 #define REG_INPUT			(*((volatile uint16*)(IO + 0x0130)))
 
 // Viewport
-#define HEIGHT 		160
+#define HEIGHT		160
 #define WIDTH 		240
-#define HALF_HEIGHT 80
-#define HALF_WIDTH 	120
+#define HALF_HEIGHT	80
+#define HALF_WIDTH	120
 
 // Colours
-#define RED 	0x001F
-#define GREEN 	0x03E0
-#define BLUE 	0x7C00
-#define WHITE 	0xFFFF
-#define BLACK 	0x0000
+#define RED		0x001F
+#define GREEN	0x03E0
+#define BLUE	0x7C00
+#define WHITE	0xFFFF
+#define BLACK	0x0000
 
 // Input
 #define KEY_UP		0x0040
@@ -51,16 +51,16 @@
 	attribute 1: x pos, obj size
 	attribute 2: obj base tile index, colour palette
 */
-#define OBJ_MASK_ATTR0_Y 0x0FF
-#define OBJ_MASK_ATTR0_X 0x1FF
+#define OBJ_MASK_ATTR0_Y	0x0FF
+#define OBJ_MASK_ATTR1_X	0x1FF
 
-#define OAM_ATTR_LAYOUT 	((volatile obj_attributes*)OAM)
-#define TILE_LAYOUT 		((volatile obj_attributes*)VRAM)
-#define OBJ_PALETTE_LAYOUT 	((volatile rgb15*)COL_PAL + 0x200)
+#define OAM_ATTR_LAYOUT		((volatile obj_attributes*)OAM)
+#define TILE_LAYOUT			((volatile tile_block*)VRAM)
+#define OBJ_PALETTE_LAYOUT	((volatile rgb15*)(COL_PAL + 0x200))
 
 // Functions
 
-#define ALLOCATE(_add, _ty)\
+#define ALLOCATE(_add,	_ty)\
 		_add = malloc(sizeof(_ty))
 
 #define DEALLOCATE(_add)\
